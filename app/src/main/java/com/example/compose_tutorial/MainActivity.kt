a577bf155eddfab8c6a59591188fb02979a1e423
Zelina <9786@live.hk>
2022-06-07 16:24:33 +0800

    lazy column

--- app/src/main/java/com/example/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/compose_tutorial/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.compose_tutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,12 +20,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose_tutorial.ui.theme.Compose_tutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreviewMessage()
+            Compose_tutorialTheme {
+                PreviewMessage()
+            }
         }
     }
 }
@@ -29,30 +39,56 @@ data class Message(
 )
 
 @Composable
-fun MyImage(){
+fun MyImage() {
     Image(
-        painter= painterResource(id = R.drawable.ic_launcher_background),
+        painter = painterResource(id = R.drawable.ic_launcher_background),
         contentDescription = "image",
         modifier = Modifier
             .size(40.dp)
             .clip(CircleShape)
+            .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
     )
 }
 
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        MyImage()
-        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
-            Text(text = "Hello ${msg.author}")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Hello ${msg.body}")
+fun Conversation(messages:List<Message>){
+    LazyColumn{
+        items(messages){
+            message->
+            MessageCard(msg = message)
         }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)
+        .clickable { println("HI")  }) {
+        MyImage()
+        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+            Text(text = "Hello ${msg.author}")
+            Spacer(modifier = Modifier.height(8.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 4.dp) {
+                Text(text = "Hell ${msg.body}",
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.padding(all = 4.dp))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
 @Composable
 fun PreviewMessage() {
-    MessageCard(msg = Message(author = "adf", body = "this is some text"))
+    Compose_tutorialTheme {
+        Conversation(messages = listOf(
+            Message(author = "adf", body = "this is some text"),
+            Message(author = "adf", body = "this is some text"),
+            Message(author = "adf", body = "this is some text"),
+            Message(author = "adf", body = "this is some text"),
+        ))
+    }
 }
